33fd5d515f0a55fc9741a5d2378df64d2bb31ef3
İrfan TARI <irfan.tari@simternet.com>
2018-09-13 14:20:40 +0300

    add turkish localization

---
+++ src/webparts/simplePoll/loc/tr-tr.js
@@ -0,0 +1,26 @@
+define([], function() {
+  return {
+    "PropertyPaneDescription": "Uygulama Ayarları",
+    "BasicGroupName": "Genel ayarlar",
+    "FontFieldLabel": "Yazı tipi",
+    "FontSizeFieldLabel": "Yazı büyüklüğü",
+    "ColorFieldLabel": "Renk",
+    "EffectGroupName": "Anket Ayarları",
+    "Answers": "Cevaplar",
+    "ManageAnswers": "Cevapları yönet",
+    "surveyList": "Anket listesi",
+    "ErrorNoItems": "No items found in the current library. Please insert inside or select another library.",
+    "ErrorSelectList": "Please edit the webpart and select a survey list. If the survey dropdown list is empty, please create a new survey Library in the current web site and create at least one question.",
+    "chartType": "Anket sonuçlarının gösterim şekli",
+    "forceVoteToViewResults": "Sonuçları görmek için anket cevaplansın",
+    "TankYou": "Teşekkürler",
+    "Recorded": "Ankete katılımınız sağlanmıştır, teşekkür ederiz!",
+    "OK": "Tamam",
+    "Error": "Hata",
+    "SelectVote": "Devam edebilmek içim bir cevap seçmelisiniz",
+    "Vote": "Tamamla",
+    "ViewResults": "Anket sonuçlarını görüntüle",
+    "AlreadyVote": "Bu anketi zaten tamamladınız, teşekkürler!",
+    "Back": "Geri"
+  }
+});
